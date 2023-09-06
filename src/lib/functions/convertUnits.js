8dee3104c999a532ec98df73d8327b24d4d071d0
Jim <jim.strother@gmail.com>
2023-09-06 17:40:51 -0500

    Input fields toggling as expected

---
+++ src/lib/functions/convertUnits.js
@@ -0,0 +1,41 @@
+export default function convertUnits(chosenUnit, measurement) {
+  // this function will convert all other units into inches for use elsewhere and to make things simpler
+  if (chosenUnit === 'inches') {
+    return {
+      unit: 'inches',
+      value: measurement
+    };
+  }
+
+  if (chosenUnit === 'centimeters') {
+    return {
+      unit: 'centimeters',
+      value: measurement * 0.393701
+    };
+  }
+
+  if (chosenUnit === 'feet') {
+    let tempValue = measurement *= 12;
+
+    if (measurement % 12 === 0) {
+      return {
+        units: 'feet',
+        value: {
+          feet: tempValue,
+          inches: 0
+        }
+      };
+    }
+    
+    let feet = Math.trunc(tempValue);
+    let inches = Math.trunc((tempValue - feet) * 12);
+
+    return {
+      unit: 'feet',
+      value: {
+        feet,
+        inches
+      }
+    };
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-  import hudsonValley from "$lib/img/Hudson-Valley--Oct-2022.jpg";
-  import { onMount } from "svelte";
+  import hudsonValley from '$lib/img/Hudson-Valley--Oct-2022.jpg';
+  import { onMount } from 'svelte';
+
+  import convertUnits from '$lib/functions/convertUnits.js';
 
   let artArea;
-  let startX;
   let displayImage;
   let height;
   let width;
+  let unit = 'inches';
+  let inches = true;
+  let feet = false;
+  let centimeters = false;
 
   onMount(() => {
     displayImage = document.querySelector('img');
@@ -19,10 +24,11 @@
 
   function changeSize(event) {
     let value = event.target.value;
+    let id = event.target.id;
 
-    if (event.target.id === "height") {
+    if (id === 'height') { // if unit === feet, need to capture inches also
       height = value;
-    } else if (event.target.id === "width") {
+    } else if (id === 'width') {
       width = value;
     }
 
@@ -30,55 +36,122 @@
     artArea.style.height = displayImage.style.height = `${height}px`;
   }
 
-  function handleMouseMove(event) {
-    startX = event.clientWidth / 2;
-    const currentX = event.clientX;
-    const deltaX = currentX - startX;
-    displayImage.style.left = `${deltaX}px`;
-    // console.log(`displayImage.style.left (mouseMove): ${displayImage.style.left}`);
+  function setUnit(event) {
+    switch (event.target.id) {
+      case 'inches':
+        unit = 'inches';
+        inches = true;
+        feet = centimeters = false;
+        toggleVisibility();
+        break;
+      case 'feet':
+        unit = 'feet';
+        feet = true;
+        inches = centimeters = false;
+        toggleVisibility();
+        break;
+      case 'centimeters':
+        unit = 'centimeters';
+        centimeters = true;
+        feet = inches = false;
+        toggleVisibility();
+        break;
+    }
   }
 
-  function handleMouseDown(event) {
-    startX = event.clientX - displayImage.clientWidth;
-    displayImage.setAttribute('draggable', true);
-    console.log(`draggable: ${displayImage.getAttribute('draggable')}`);
-    // console.log(`startX (mouseDown): ${startX}`);
+  function toggleVisibility() {
+    let elements = document.getElementsByClassName('visible');
+    
+    for (let i = 0; i < elements.length; i++) {
+      const style = elements[i].style;
+      style.display = feet ? 'block' : 'none';
+    }
   }
 
-  function handleMouseUp() {
-    displayImage.setAttribute('draggable', false);
-    console.log(`draggable: ${displayImage.getAttribute('draggable')}`);
-  }
+  
+
+  // Need to have three buttons to change between 'inches', 'feet & inches', and 'centimeters'
+  // Need a function to calculate the total area and price based upon that.
 </script>
 
 <h1>Area Public Visualizer</h1>
 
 <form>
-  <label for="height" class="measurement">Height</label>
-  <input id="height" type="text" placeholder="Height" on:input={changeSize} />
-  <label for="width" class="measurement">Width</label>
-  <input id="width" type="text" placeholder="Width" on:input={changeSize} />
+  <div class='buttons'>
+    <button class='units' id='inches' on:click={setUnit}>Inches</button>
+    <button class='units' id='feet' on:click={setUnit}>Feet & Inches</button>
+    <button class='units' id='centimeters' on:click={setUnit}>Centimeters</button>
+  </div>
+  <div class='inputs'>
+    <div class='height'>
+      <label for='height' class='measurement'>Height({unit}): </label>
+      <input id='height' type='text' on:input={changeSize} />
+
+      <label for='height' class='measurement visible'>Height(inches): </label>
+      <input id='height-inches' class='visible' type='text' />
+    </div>
+    <div class='width'>
+      <label for='width' class='measurement'>Width({unit}): </label>
+      <input id='width' type='text' on:input={changeSize} />
+
+      <label for='width' class='measurement visible'>Width(inches): </label>
+      <input id='width-inches' class='visible' type='text' />
+    </div>
+  </div>
 </form>
 
-<div class="art-area" bind:this={artArea} {height}>
+<div class='art-area' bind:this={artArea} {height}>
   <img
     src={hudsonValley}
-    alt="Beautiful Hudson Valley"
+    alt='Beautiful Hudson Valley'
     {height}
-    aria-hidden="true"
-    on:mousedown={handleMouseDown}
-    on:mousemove={handleMouseMove}
-    on:mouseUp={handleMouseUp}
   />
 </div>
 
 <style>
+  * {
+    border: 1px solid red;
+  }
   .art-area {
     border: 1px dotted black;
     z-index: 100;
     position: relative;
     overflow: hidden;
     margin-top: 2em;
+    max-width: 80%;
+  }
+
+  form {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-around;
+    align-content: space-between;
+    max-width: 80%;
+  }
+
+  .buttons {
+    align-self: center;
+    max-width: 25em;
+  }
+
+  button {
+    margin: 1em;
+  }
+
+  .inputs {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin: 1em;
+  }
+
+  .height, .width {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .visible {
+    display: none;
   }
   
   img {
