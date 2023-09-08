aa56c59ca3e3d121925ae16510bd75b3ce3c5b3a
Jim <jim.strother@gmail.com>
2023-09-08 12:25:13 -0500

    demo finished and ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,20 @@
   import { onMount } from 'svelte';
 
   const cmToInch = 0.393701;
+  const priceSqFt = 7.95;
+  const order = {
+    height: 0,
+    width: 0,
+    totalArea: 0,
+    price: 0,
+  };
 
   let artArea;
   let displayImage;
-
-  let height;
+  let totalArea = 0;
+  let height = 0;
   let height2 = 0;
-  let width;
+  let width = 0;
   let width2 = 0;
 
   let viewHeight = '500px';
@@ -22,7 +29,7 @@
 
   onMount(() => {
     displayImage = document.querySelector('img');
-    artArea = document.querySelector('.art-area');
+    artArea = document.querySelector('.art-area');   
 
     artArea.style.width = viewWidth;
     artArea.style.height = viewHeight;
@@ -32,6 +39,8 @@
   function changeSize(event) {
     let value = Number(event.target.value);
     let id = String(event.target.id);
+    let tempHeight = 0;
+    let tempWidth = 0;
 
     switch (id) {
       case 'height':
@@ -47,29 +56,46 @@
         width2 = value;
         break;
     }
-    
-    switch (unit) {
-      case 'inches':
-        viewHeight = `${convertToPx(height)}px`;
-        viewWidth = `${convertToPx(width)}px`;
-        break;
-      case 'centimeters':
-        viewHeight = `${convertToPx(height * cmToInch)}px`;
-        viewWidth = `${convertToPx(width * cmToInch)}px`;
-        break;
-      case 'feet':
-        let tempHeight = height2 + (height * 12);
-        let tempWidth = width2 + (width * 12);
-        console.log(`temp: ${tempHeight} x ${tempWidth}`);
-        viewHeight = `${convertToPx(tempHeight)}px`;
-        viewWidth = `${convertToPx(tempWidth)}px`;
-        console.log(`view: ${viewHeight} x ${viewWidth}`);
-        break;
-    }
 
-    artArea.style.width = viewWidth;
-    artArea.style.height = viewHeight;
-    displayImage.style.height = viewHeight;
+    setTimeout(() => {      
+      switch (unit) {
+        case 'inches':
+          if (height > 0) viewHeight = `${convertToPx(height)}px`;
+          if (width > 0) viewWidth = `${convertToPx(width)}px`;
+          totalArea = height * width !== 0 ? Math.round((height / 12) * (width / 12)) : 0;
+          order.height = `${height} in.`;
+          order.width = `${width} in.`;
+          order.totalArea = `${totalArea} sq ft`;
+          order.price = `$${totalArea * priceSqFt}`;
+          break;
+        case 'centimeters':
+          tempHeight = height * cmToInch;;
+          tempWidth = width * cmToInch;
+          if (height > 0) viewHeight = `${convertToPx(tempHeight)}px`;
+          if (width > 0) viewWidth = `${convertToPx(tempWidth)}px`;
+          totalArea = tempHeight * tempWidth !== 0 ? Math.round((tempHeight / 12) * (tempWidth / 12)) : 0;
+          order.height = `${height} in.`;
+          order.width = `${width} in.`;
+          order.totalArea = `${totalArea} sq ft`;
+          order.price = `$${totalArea * priceSqFt}`;
+          break;
+        case 'feet':
+          tempHeight = height2 + (height * 12);
+          tempWidth = width2 + (width * 12);
+          if (height > 0) viewHeight = `${convertToPx(tempHeight)}px`;
+          if (width > 0) viewWidth = `${convertToPx(tempWidth)}px`;
+          totalArea = tempHeight * tempWidth !== 0 ? Math.round((tempHeight / 12) * (tempWidth / 12)) : 0;
+          order.height = `${height} in.`;
+          order.width = `${width} in.`;
+          order.totalArea = `${totalArea} sq ft`;
+          order.price = `$${totalArea * priceSqFt}`;
+          break;
+      }
+
+      artArea.style.width = viewWidth;
+      artArea.style.height = viewHeight;
+      displayImage.style.height = viewHeight;
+    }, 1000)
 
     function convertToPx(value) {
       return value * 3;
@@ -82,6 +108,9 @@
         unit = 'inches';
         inches = true;
         feet = centimeters = false;
+        event.target.disabled = true;
+        document.getElementById('feet').disabled = false;
+        document.getElementById('centimeters').disabled = false;
         clearFields();
         toggleVisibility();
         break;
@@ -89,6 +118,9 @@
         unit = 'feet';
         feet = true;
         inches = centimeters = false;
+        event.target.disabled = true;
+        document.getElementById('inches').disabled = false;
+        document.getElementById('centimeters').disabled = false;
         clearFields();
         toggleVisibility();
         break;
@@ -96,6 +128,9 @@
         unit = 'centimeters';
         centimeters = true;
         feet = inches = false;
+        event.target.disabled = true;
+        document.getElementById('feet').disabled = false;
+        document.getElementById('inches').disabled = false;
         clearFields();
         toggleVisibility();
         break;
@@ -118,52 +153,65 @@
       inputs[i].value = '';
     }
 
+    totalArea = 0;
+    height = 0;
+    height2 = 0;
+    width = 0;
+    width2 = 0;
+
     displayImage.style.height = '500px';
     artArea.style.height = '500px';
     artArea.style.width = '1000px';
   }
-
-  // Need to have three buttons to change between 'inches', 'feet & inches', and 'centimeters'
-  // Need a function to calculate the total area and price based upon that.
 </script>
 
 <div class="visualizer">
   <h1>Area Public Visualizer</h1>
 
   <div class="art-container">
-    <div class='art-area' bind:this={artArea}>
-      <img
-        src={mural}
-        alt='Wonderful Art!'
-      />
+    <div class="art-holder">
+      <div class='art-area' bind:this={artArea}>
+        <img
+          src={mural}
+          alt='Wonderful Art!'
+        />
+      </div>
     </div>
-    <form>      
-      <div class='inputs'>
-        <div class='height'>
-          <h4>Height</h4>
-          <input id='height' type='text' on:input={changeSize} placeholder={unit} />   
-          <input id='height-inches' class='visible' type='text' on:input={changeSize} placeholder="inches" />
+    <div class="show">
+      <form>      
+        <div class='inputs'>
+          <div class='height'>
+            <h4>Height</h4>
+            <input id='height' type='text' on:input={changeSize} placeholder={unit} />   
+            <input id='height-inches' class='visible' type='text' on:input={changeSize} placeholder="inches" />
+          </div>
+          <div class='width'>
+            <h4>Width</h4>
+            <input id='width' type='text' on:input={changeSize} placeholder={unit} />    
+            <input id='width-inches' class='visible' type='text' on:input={changeSize} placeholder="inches" />
+          </div>
         </div>
-        <div class='width'>
-          <h4>Width</h4>
-          <input id='width' type='text' on:input={changeSize} placeholder={unit} />    
-          <input id='width-inches' class='visible' type='text' on:input={changeSize} placeholder="inches" />
+        <div class='buttons'>
+          <button class='units' id='inches' on:click={setUnit} disabled>Inches</button>
+          <button class='units' id='feet' on:click={setUnit}>Feet & Inches</button>
+          <button class='units' id='centimeters' on:click={setUnit}>Centimeters</button>
         </div>
+      </form>
+      <div class="price">
+        {#if totalArea}
+        <h3>Total: ${(totalArea * priceSqFt).toFixed(2)}</h3>
+        <p>{totalArea} sq ft</p>
+        <p>${priceSqFt} per square foot</p>
+        <button class="clear" on:click={clearFields}>Clear Fields</button>
+        {:else}
+        <p>Enter your dimensions above and your total will show here.</p>
+        {/if}
       </div>
-      <div class='buttons'>
-        <button class='units' id='inches' on:click={setUnit}>Inches</button>
-        <button class='units' id='feet' on:click={setUnit}>Feet & Inches</button>
-        <button class='units' id='centimeters' on:click={setUnit}>Centimeters</button>
-      </div>
-    </form>
+    </div>
   </div>
 </div>
 
 <style>
-  * {
-    /* border: 1px solid red; */
-  }
-
   .visualizer {
     display: flex;
     flex-flow: column nowrap;
@@ -171,12 +219,18 @@
 
   h1 {
     align-self: center;
-    margin-left: -10em;
+    border-bottom: 1px solid black;
+  }  
+
+  .art-holder {
+    display: flex;
+    justify-content: center;
+    width: 80em;
   }
 
   .art-container {
     display: flex;
-    flex-flow: row nowrap;
+    flex-flow: row wrap;
     justify-content: space-around;
   }
   .art-area {
@@ -201,6 +255,7 @@
     align-content: space-between;
     align-self: center;
     max-width: 80%;
+    margin-left: 1.5em;
   }
 
   .buttons {
@@ -209,7 +264,7 @@
   }
 
   button {
-    margin: 1em;
+    margin: 0.5em;
   }
 
   .inputs {
@@ -231,5 +286,26 @@
 
   .visible {
     display: none;
+  }
+
+  .show {
+    display: flex;
+    flex-flow: column nowrap;
+    align-self: center;
+    margin-right: 3em;
+  }
+  
+  .price {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    height: 10em;
+    width: 25em;
+  }
+
+  .clear {
+    align-self: center;
   }
 </style>
